53c797126a465821e8dabbd2f68db2614c30d9e8
Lorenzo La Gorce <lorenzo.g@horizon-web.io>
2023-08-17 17:21:14 +0200

    update 4

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
 	transpileDependencies: true,
-	publicPath: "/dist/",
+
+	outputDir: "../dist",
+	assetsDir: "media",
+	indexPath: "index.html",
 });
