4b59807f89dd389761873fbf14712fc0dcb393f1
Lorenzo La Gorce <lorenzo.g@horizon-web.io>
2023-08-17 15:15:22 +0200

    asdf

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: "/Shopping-Website/",
+	transpileDependencies: true,
+	publicPath: "/dist/",
 });
